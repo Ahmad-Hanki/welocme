5050e5aacc46a46df5879a069e45555d45d8fd8a
Ahmad <itxti909@gmail.com>
2025-02-17 19:41:04 +0300

    3 lessions

---
+++ app/(coffee)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Slot } from 'expo-router'; //children
+import React from 'react';
+import { StyleSheet, View,  } from 'react-native';
+
+const CoffeeLAyout = () => {
+    return (
+        <Slot /> ///children
+    );
+}
+
+
+export default CoffeeLAyout;
