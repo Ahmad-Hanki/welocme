87bc3402b46fedd3b61b7d7ac38d5f371828f505
Ahmad <itxti909@gmail.com>
2025-02-18 11:24:22 +0300

    new start

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,20 @@
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
-import { useColorScheme } from "@/hooks/useColorScheme";
-import { Appearance } from "react-native";
-import { Colors } from "@/constants/Colors";
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = Appearance.getColorScheme();
-  const theme = colorScheme === "dark" ? Colors.dark : Colors.light;
-
+  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
   useEffect(() => {
@@ -29,20 +28,12 @@ export default function RootLayout() {
   }
 
   return (
-    <>
-      <Stack
-        screenOptions={{
-          headerStyle: { backgroundColor: theme.headerBackground },
-          headerTintColor: theme.text,
-          headerShadowVisible: false,
-        }}
-      >
-        <Stack.Screen name="index" options={{headerShown:false, title:'Home'}} />
-        <Stack.Screen name="menu" options={{headerShown:true, title:'Menu', headerTitle:'Coffee Shop Menu', }} />
-        <Stack.Screen name="contact" options={{headerShown:true, title:'Contact', headerTitle:'Contact Us', }} />
-        <Stack.Screen name="+not-found" options = {{headerShown:false}}/>\
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
-    </>
+    </ThemeProvider>
   );
 }
